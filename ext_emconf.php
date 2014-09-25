865b13b8030e41be9725ae10c02ee4655768d315
Tim Lochmüller <tim@fruit-lab.de>
2014-09-25 09:04:54 +0200

    [BUGFIX] Add description
    Add the information, that the extension is not class loader
    or a PHP SPL autoloader.
    
    Resolves: #61846
    Change-Id: Ic6344eef488d506ce1eb707e6b2978dde0f7985c
    Reviewed-on: http://review.typo3.org/32964
    Reviewed-by: Tim Lochmüller <tim@fruit-lab.de>
    Tested-by: Tim Lochmüller <tim@fruit-lab.de>

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@
 /** @var $_EXTKEY string */
 $EM_CONF[$_EXTKEY] = array(
 	'title'              => 'Autoloader',
-	'description'        => 'Automatic components loading of ExtBase extensions to get more time for coffee in the company ;) Loads CommandController, Xclass, Hooks, Aspects, FlexForms, Slots...',
+	'description'        => 'Automatic components loading of ExtBase extensions to get more time for coffee in the company ;) This ext is not a PHP SPL autoloader or class loader - it is better! Loads CommandController, Xclass, Hooks, Aspects, FlexForms, Slots...',
 	'version'            => '1.2.5',
 	'state'              => 'beta',
 	'clearcacheonload'   => 1,
