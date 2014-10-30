8c1a885559b7ca7c08c4227325fb2f0cf7a918c9
Tim Lochmüller <tim@fruit-lab.de>
2014-10-30 21:18:59 +0100

    [TASK] Release version 1.3.1
    Change-Id: I76da1c8ec9642ce21b81538b166e349b3ac8a1f4
    Reviewed-on: http://review.typo3.org/33641
    Reviewed-by: Tim Lochmüller <tim@fruit-lab.de>
    Tested-by: Tim Lochmüller <tim@fruit-lab.de>

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@
 $EM_CONF[$_EXTKEY] = array(
 	'title'              => 'Autoloader',
 	'description'        => 'Automatic components loading of ExtBase extensions to get more time for coffee in the company ;) This ext is not a PHP SPL autoloader or class loader - it is better! Loads CommandController, Xclass, Hooks, Aspects, FlexForms, Slots...',
-	'version'            => '1.3.0',
+	'version'            => '1.3.1',
 	'state'              => 'beta',
 	'clearcacheonload'   => 1,
 	'author'             => 'Tim Lochmüller',
