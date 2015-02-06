bafd2b701eda1ac914e201e700e7b1a0ee12f19a
Tim Lochmüller <tim@fruit-lab.de>
2015-02-06 08:09:58 +0100

    [TASK] Release 1.5.5
    Change-Id: I30df2b805c01d6444da9ba1f93a4f96bae7ec4db
    Reviewed-on: http://review.typo3.org/36703
    Reviewed-by: Tim Lochmüller <tim@fruit-lab.de>
    Tested-by: Tim Lochmüller <tim@fruit-lab.de>

--- Resources/Private/Examples/autoloader_aspect/ext_emconf.php
+++ Resources/Private/Examples/autoloader_aspect/ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => '',
 	'constraints' => array(
 		'depends' => array(
-			'autoloader' => '1.5.4-9.9.9',
+			'autoloader' => '1.5.5-9.9.9',
 		),
 	),
 );
