129a2c000f0174d4045fe37716d36f92cbb1d556
Tim Lochmüller <tim@fruit-lab.de>
2015-04-21 11:26:30 +0200

    [RELEASE] Release 1.5.7
    Change-Id: I194b1ee8098c1c68511f805c01b4985b0021bdb0
    Reviewed-on: http://review.typo3.org/38845
    Reviewed-by: Tim Lochmüller <tim@fruit-lab.de>
    Tested-by: Tim Lochmüller <tim@fruit-lab.de>

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,21 +8,18 @@
  */
 
 /** @var $_EXTKEY string */
-$EM_CONF[$_EXTKEY] = [
+$EM_CONF[$_EXTKEY] = array(
 	'title'            => 'Autoloader',
 	'description'      => 'Automatic components loading of ExtBase extensions to get more time for coffee in the company ;) This ext is not a PHP SPL autoloader or class loader - it is better! Loads CommandController, Xclass, Hooks, Aspects, FlexForms, Slots...',
-	'version'          => '1.5.6',
+	'version'          => '1.5.7',
 	'state'            => 'stable',
 	'clearcacheonload' => 1,
 	'author'           => 'Tim Lochmüller',
 	'author_email'     => 'tim.lochmueller@hdnet.de',
 	'author_company'   => 'hdnet.de',
-	'constraints'      => [
-		'depends' => [
-			'typo3'   => '6.2.0-7.1.99',
-			'backend' => '6.2.0-7.1.99',
-			'core'    => '6.2.0-7.1.99',
-			'extbase' => '6.2.0-7.1.99',
-		],
-	],
-];
+	'constraints'      => array(
+		'depends' => array(
+			'typo3' => '6.2.0-7.2.99',
+		),
+	),
+);
